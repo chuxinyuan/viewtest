eea74b99897ff88373bf709acb2a3b31d96e11ff
chuxinyuan <chuxinyuan@outlook.com>
2025-09-24 19:13:03 +0800

    update

--- server.R
+++ server.R
@@ -11,42 +11,43 @@ server = function(input, output, session) {
         tags$div(class = "spinner-border", role = "status")
       )
       
+      # 创建临时 Rmd 文件并写入用户输入内容
+      temp_file = tempfile(fileext = ".Rmd")
+      on.exit(unlink(temp_file), add = TRUE)
+      yaml = c("---\noutput: html\n---\n\n", "")
+      content = paste0(yaml[2], input$content)
+      writeLines(content, temp_file)
+      litedown:::convert_knitr(temp_file)
+
+      # 渲染 Rmd 为 HTML
+      temp_html = tempfile(fileext = ".html")
+      on.exit(unlink(temp_file), add = TRUE)
+      litedown::fuse(temp_file, temp_html, quiet = TRUE)
+      
       # # 创建临时 Rmd 文件
       # temp_file = tempfile(fileext = ".Rmd")
+      # on.exit(unlink(temp_file), add = TRUE)
       # writeLines(input$content, temp_file)
-      # litedown:::convert_knitr(temp_file)
       # on.exit(unlink(temp_file), add = TRUE)
       # 
       # # 渲染 Rmd 文件为 HTML
       # temp_html = tempfile(fileext = ".html")
-      # litedown::fuse(
+      # on.exit(unlink(temp_html), add = TRUE)
+      # rmarkdown::render(
       #   input = temp_file,
-      #   output = temp_html,
+      #   output_file = temp_html,
+      #   output_options = list(highlight = 'kate'),
       #   quiet = TRUE
       # )
-      # on.exit(unlink(temp_html), add = TRUE)
       
-      # 创建临时 Rmd 文件
-      temp_file = tempfile(fileext = ".Rmd")
-      writeLines(input$content, temp_file)
-      on.exit(unlink(temp_file), add = TRUE)
-      
-      # 渲染 Rmd 文件为 HTML
-      temp_html = tempfile(fileext = ".html")
-      rmarkdown::render(
-        input = temp_file,
-        output_file = temp_html,
-        output_options = list(highlight = 'zenburn'),
-        quiet = TRUE
+      # 读取并返回 HTML 内容
+      html_content = paste(
+        readLines(temp_html, warn = FALSE),
+        collapse = "\n"
       )
-      on.exit(unlink(temp_html), add = TRUE)
       
       # 渲染 HTML 文件，加入 preview id 方便定制
-      html_content = readLines(temp_html, warn = FALSE)
-      tags$div(
-        id = "preview",
-        HTML(paste(html_content, collapse = "\n"))
-      )
+      tags$div(id = "preview", HTML(html_content))
     }, error = function(e) {
       # 显示错误信息
       tags$div(
